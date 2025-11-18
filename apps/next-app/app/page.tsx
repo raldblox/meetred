127158f9101b6ef4c918eb3bbb729e46b37fdf70
raldblox <raldblox@outlook.com>
2025-11-19 00:33:25 +0800

    Revert "init: electron-app"
    This reverts commit ac80ba2c045f70cbe0d31d293201df17e0795a38.

--- apps/next-app/app/page.tsx
+++ apps/next-app/app/page.tsx
@@ -1,35 +1,5 @@
-"use client";
-
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-
 export default function Home() {
-  const [roomId, setRoomId] = useState("");
-  const router = useRouter();
-
-  const handleJoinRoom = () => {
-    if (roomId) {
-      router.push(`/room/${roomId}`);
-    }
-  };
-
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="flex flex-col items-center justify-center gap-2">
-        <input
-          type="text"
-          value={roomId}
-          onChange={(e) => setRoomId(e.target.value)}
-          placeholder="Enter Room ID"
-          className="px-4 py-2 border rounded-md"
-        />
-        <button
-          onClick={handleJoinRoom}
-          className="px-4 py-2 text-white bg-blue-500 rounded-md"
-        >
-          Join Room
-        </button>
-      </div>
-    </section>
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10" />
   );
 }
